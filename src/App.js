e34889e85d5348940fff3dd0536e221cc84bf36c
Said Oudou <>
2021-03-26 21:30:29 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Einführung in React</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button></Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
