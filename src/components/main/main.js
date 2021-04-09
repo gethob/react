6679239985b4d7b3759bb9fa1eac4c820d0be8b7
Said Oudou <>
2021-04-09 23:44:04 +0200

    restaurant

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,21 +1,35 @@
+import React from 'react';
+import './main.css'
+import Soup from '../../images/soup.jpg'
+const Main = (props) =>{
 
-        import React from 'react';
-        const Main = (props) =>{
 
           return(
             <div>
               <header>
-              Here goes logo, navigation, etc.
+
+              <nav>
+                 <a href="#">Home</a>
+                 <a href="#">Services</a>
+                 <a href="#">Contact</a>
+                 <a href="#">About Us</a>
+              </nav>
+
               </header>
-              <main>
-                A place for website's main content
+
+              <main style={ {height: '70vh'}   }>
+              <img src={Soup} width={140} height={100}/>
+              <p><span>Preis: € 5</span></p>
+              <p>Suppe</p>
+
               </main>
+
               <footer>
                 Footer information, links, etc.
               </footer>
+
             </div>
           )
+}
 
-        }
-
-        export default Main;
+export default Main;
